8e76e629ee2feca8cc4999b352a02ae87b9be9d6
Clement <siritualnoise23@gmail.com>
2024-05-18 07:45:07 +0300

    Implement auth, appwrite and sentry

--- constants/index.ts
+++ constants/index.ts
@@ -16,7 +16,7 @@ export const sidebarLinks = [
   },
   {
     imgURL: "/icons/money-send.svg",
-    route: "/payment-transfer",
+    route: "/pa",
     label: "Transfer Funds",
   },
 ];
---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ec358c78f5ae6e5e1ba851acd7c23d8b@o4507275729240064.ingest.us.sentry.io/4507275730681856",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+});
